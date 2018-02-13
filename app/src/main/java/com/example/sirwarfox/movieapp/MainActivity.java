56a6b43807ad94c64bc622333ef83ba2e33f664f
Sir.WarFox <hweitani@gmail.com>
2018-02-13 18:38:48 +0700

    Recycler View

--- app/src/main/java/com/example/sirwarfox/movieapp/MainActivity.java
+++ app/src/main/java/com/example/sirwarfox/movieapp/MainActivity.java
@@ -11,6 +11,9 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.sirwarfox.movieapp.adapter.MoviesAdapter;
@@ -99,19 +102,46 @@ public class MainActivity extends AppCompatActivity {
 
             Client client = new Client();
             Service apiService = Client.getClient().create(Service.class);
-//38.33
+
             Call<MoviesResponse> call = apiService.getPopularMovies(BuildConfig.THE_MOVIE_DB_API_TOKEN);
             call.enqueue(new Callback<MoviesResponse>() {
                 @Override
                 public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
-
+                    List<Movie> movies = response.body().getResults();
+                    recyclerView.setAdapter(new MoviesAdapter(getApplicationContext(),movies));
+                    recyclerView.smoothScrollToPosition(0);
+                    if(swipeContainer.isRefreshing()){
+                        swipeContainer.setRefreshing(false);
+                    }
+                    pd.dismiss();
                 }
 
                 @Override
                 public void onFailure(Call<MoviesResponse> call, Throwable t) {
-
+                    Log.d("ERROR", t.getMessage());
+                    Toast.makeText(MainActivity.this, "Error Fetching Data", Toast.LENGTH_SHORT).show();
                 }
             });
+        }catch (Exception e){
+            Log.d("ERROR", e.getMessage());
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_settings:
+                return true;
+            default:
+                    return super.onOptionsItemSelected(item);
+        }
+
     }
 }
--- app/src/main/java/com/example/sirwarfox/movieapp/adapter/MoviesAdapter.java
+++ app/src/main/java/com/example/sirwarfox/movieapp/adapter/MoviesAdapter.java
@@ -1,8 +1,89 @@
 package com.example.sirwarfox.movieapp.adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.sirwarfox.movieapp.DetailActivity;
+import com.example.sirwarfox.movieapp.R;
+import com.example.sirwarfox.movieapp.model.Movie;
+
+import java.util.List;
+
 /**
  * Created by Sir.WarFox on 11/02/2018.
  */
 
-public class MoviesAdapter {
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<Movie> movieList;
+
+    public MoviesAdapter(Context mContext, List<Movie> movieList) {
+        this.mContext = mContext;
+        this.movieList = movieList;
+    }
+
+    @Override
+    public MoviesAdapter.MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.movie_card , viewGroup , false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final MoviesAdapter.MyViewHolder holder, int position) {
+        holder.title.setText(movieList.get(position).getOriginal_title());
+        String vote = Double.toString(movieList.get(position).getVoteAvarage());
+        holder.userRating.setText(vote);
+
+        Glide.with(mContext)
+                .load(movieList.get(position).getPosterPath())
+                .placeholder(R.drawable.load)
+                .into(holder.thumbnail);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public TextView title, userRating;
+        public ImageView thumbnail;
+
+        public  MyViewHolder(View view){
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            userRating = (TextView) view.findViewById(R.id.userRating);
+            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int pos = getAdapterPosition();
+                    if(pos != RecyclerView.NO_POSITION){
+                        Movie clickedDataItems = movieList.get(pos);
+                        Intent intent = new Intent(mContext, DetailActivity.class);
+                        intent.putExtra("original_title", movieList.get(pos).getOriginal_title());
+                        intent.putExtra("poster_path", movieList.get(pos).getPosterPath());
+                        intent.putExtra("overview", movieList.get(pos).getOverview());
+                        intent.putExtra("vote_average", Double.toString(movieList.get(pos).getVoteAvarage()));
+                        intent.putExtra("release_date", movieList.get(pos).getRelease_date());
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        mContext.startActivity(intent);
+                        Toast.makeText(view.getContext(), "You Clicked "+ clickedDataItems.getOriginal_title(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
 }
